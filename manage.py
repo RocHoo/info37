05752d02d8f7e238d185ed9002e0ee33e91cec75
hoo <hoo@qq.com>
2019-01-16 13:24:57 +0800

    目录搭建，实现状态保持，session信息存储在redis

--- manage.py
+++ manage.py
@@ -1,11 +1,27 @@
-from flask import Flask
+from flask import Flask,session
 
+from flask_session import Session
+
+from redis import StrictRedis
 
 app=Flask(__name__)
+
+app.config['SECRET_KEY']='dxdvvrEG3IvY8kz1FyE7rYOZexjl+IsiSD8EBo8J7iEMwQ4pLuY8pg=='
+
+app.config['SESSION_TYPE']='redis'
+
+app.config['SESSION_REDIS']=StrictRedis(host='127.0.0.1',port=6379)
+
+app.config['SESSION_USE_SIGNER']=True
+
+app.config['PERMANENT_SESSION_LIFETIME']=86400
+
+Session(app)
 
 
 @app.route('/')
 def index():
+    session['itcast']='2019'
     return 'hello world2018'
 
 
